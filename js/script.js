cb87f05bac8a4147657bc4f9623f4af1130a971c
ekbhast <ekbhast@gmail.com>
2024-09-19 16:38:10 +0300

    refactoring all

--- js/script.js
+++ js/script.js
@@ -1,38 +1,97 @@
 'use strict'
-
-//variable
 const body = document.querySelector('body');
-const heroSprite = document.createElement('img');
-const heroDiv = document.createElement('div');
-let rightPosition = 0;
+// let rightPosition = 5;
+// let heroPostition = 0;
+// let interval = null;
+// let direction = null;
 
-heroSprite.src = '../assets/hero-sprite.png';
-heroSprite.classList.add('heroSprite');
+const character = {
+    sprite: '../assets/hero-sprite.png',
+    characterPosition : 0,
+    animationFrame: 5,
+    animationIntervalMovement : null,
+    direction: null,
 
-heroDiv.classList.add('heroDiv');
 
-body.append(heroDiv);
-heroDiv.append(heroSprite);
+    //render character
+    renderCharacter(){
+        const heroSprite = document.createElement('img');
+              heroSprite.src = this.sprite;
+              heroSprite.classList.add('hero-sprite');
+
+        const heroDiv = document.createElement('div');
+              heroDiv.classList.add('hero-div');
+              heroDiv.classList.add('hero-div');
+
+        body.append(heroDiv);
+        heroDiv.append(heroSprite);
+    },
+
+    //movement character
+    movement (direction){
+        rightPosition = rightPosition - 1;
+        
+        if (rightPosition < 1 ) {
+            rightPosition = 5;
+        }
+
+        heroSprite.style.left = `-${rightPosition*288}px`;
+        heroPostition = heroPostition + 20;
+        heroDiv.style.left = `${heroPostition}px`
+        console.log(heroPostition);
+    }
+
+}
 
 //functions
 
-// movement hero
-function movementLeft (){
-    rightPosition = rightPosition + 1;
 
-    if (rightPosition > 5) {
-        rightPosition = 0;
-    }
 
-    heroSprite.style.left = `-${rightPosition*288}px`;
-    // console.log('Кнопка влево');
-}
+character.renderCharacter();
 
 // event keyboard
-document.addEventListener('keydown',(KeyboardEvent) => {
-    if (KeyboardEvent.key === 'ArrowRight') {
-        movementLeft();
-    }
+document.addEventListener('keydown',(event) => {
+    if (event.repeat != true){
+        switch (event){
+            case 'ArrowRight':
+                direction = right;
+                break;
+            
+            case 'ArrowLeft':
+                direction = left;
+            break;
+        }
+    movement();
+    interval = setInterval(movement, 130);
+}
+
 });
+
+document.addEventListener('keyup', (event) =>{
+    switch (event){
+        case 'ArrowRight':
+            direction = null;
+            break;
+        
+        case 'ArrowLeft':
+            direction = null;
+        break;
+    }
+    clearInterval(interval);
+})
+
+
+//     if (event.key === 'ArrowRight' && event.repeat == false) {
+//         interval = setInterval(movementLeft, 130);
+//     }
+// });
+
+// document.addEventListener('keyup',(event) => {
+//     if (event.key === 'ArrowRight') {
+//         clearInterval(interval);
+//     }
+// });
+
+
 
 //event handler
